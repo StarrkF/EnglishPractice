867752b9f79af3b9909fb6faf7ae372552fed2b2
Muhammed Furkan Aymergen <faymergen98@gmail.com>
2022-09-12 23:12:32 +0300

    created custom cards

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="grid md:grid-cols-4 gap-3 ">
+    
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+    <div class="card">COL</div>
+   
+  
+  
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0px 0px 40px 30px rgba(0, 0, 0, 0.3)',
+      },
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+    },
+  },
+  plugins: [],
+}
